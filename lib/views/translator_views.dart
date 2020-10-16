291c7cb56bdfe4d53a8e9e7a1d8c19809d5aedea
Carlos Caravaca <ccaravac@everis.com>
2020-10-16 09:19:11 +0100

    Widgets

--- lib/views/translator_views.dart
+++ lib/views/translator_views.dart
@@ -11,7 +11,72 @@ class _TranslatorState extends State<Translator> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [TextRowHeader(), EnterText()],
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.only(
+                      left: 15.0, top: 15.0, right: 5.0, bottom: 15.0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(28, 28, 30, 0.5),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text('Spanish',
+                            style: TextStyle(fontSize: 18.0),
+                            textAlign: TextAlign.center),
+                      )),
+                )),
+                Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.only(
+                      left: 5.0, top: 15.0, right: 15.0, bottom: 15.0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(28, 28, 30, 0.5),
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(children: [
+                          Text(
+                            'English',
+                            style: TextStyle(fontSize: 18.0),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            'UK',
+                            style:
+                                TextStyle(color: Colors.grey, fontSize: 18.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        ]),
+                      )),
+                ))
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(28, 28, 30, 0.5),
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: TextField(
+                  style: TextStyle(fontSize: 24.0),
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: 'Enter text'),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: Padding(
@@ -49,89 +114,6 @@ class _TranslatorState extends State<Translator> {
             }
           });
         },
-      ),
-    );
-  }
-}
-
-class TextRowHeader extends StatelessWidget {
-  const TextRowHeader({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 25.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Expanded(
-              child: Padding(
-            padding: EdgeInsets.only(
-                left: 15.0, top: 15.0, right: 5.0, bottom: 15.0),
-            child: Container(
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(28, 28, 30, 0.5),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text('Spanish',
-                      style: TextStyle(fontSize: 18.0),
-                      textAlign: TextAlign.center),
-                )),
-          )),
-          Expanded(
-              child: Padding(
-            padding: EdgeInsets.only(
-                left: 5.0, top: 15.0, right: 15.0, bottom: 15.0),
-            child: Container(
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(28, 28, 30, 0.5),
-                    borderRadius: BorderRadius.circular(10.0)),
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Column(children: [
-                    Text(
-                      'English',
-                      style: TextStyle(fontSize: 18.0),
-                      textAlign: TextAlign.center,
-                    ),
-                    Text(
-                      'UK',
-                      style: TextStyle(color: Colors.grey, fontSize: 18.0),
-                      textAlign: TextAlign.center,
-                    ),
-                  ]),
-                )),
-          ))
-        ],
-      ),
-    );
-  }
-}
-
-class EnterText extends StatelessWidget {
-  const EnterText({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-            color: Color.fromRGBO(28, 28, 30, 0.5),
-            borderRadius: BorderRadius.circular(10.0)),
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: TextField(
-            style: TextStyle(fontSize: 24.0),
-            decoration: InputDecoration(
-                border: InputBorder.none, hintText: 'Enter text'),
-          ),
-        ),
       ),
     );
   }
