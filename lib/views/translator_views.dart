3f7bc8053d5b2983906da9087e0e7234045800b4
Carlos Caravaca <ccaravac@everis.com>
2020-10-21 20:27:23 +0100

    widget

--- lib/views/translator_views.dart
+++ lib/views/translator_views.dart
@@ -13,14 +13,14 @@ class _TranslatorState extends State<Translator> {
     Future<Null> _refresh() async {
       await Future.delayed(Duration(seconds: 1));
       setState(() {
-        ViewTranslator();
+        viewTranslator();
       });
     }
 
     return Scaffold(
         body: RefreshIndicator(
           onRefresh: _refresh,
-          child: ViewTranslator(),
+          child: viewTranslator(),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: Padding(
@@ -55,93 +55,85 @@ class _TranslatorState extends State<Translator> {
   }
 }
 
-class ViewTranslator extends StatelessWidget {
-  const ViewTranslator({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 25.0),
-          child: SingleChildScrollView(
-            physics: AlwaysScrollableScrollPhysics(),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
+Widget viewTranslator() {
+  return Column(
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(top: 25.0),
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                  child: Padding(
+                padding: EdgeInsets.only(
+                    left: 15.0, top: 15.0, right: 5.0, bottom: 15.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(28, 28, 30, 0.5),
+                        borderRadius: BorderRadius.circular(10)),
                     child: Padding(
-                  padding: EdgeInsets.only(
-                      left: 15.0, top: 15.0, right: 5.0, bottom: 15.0),
-                  child: Container(
-                      decoration: BoxDecoration(
-                          color: Color.fromRGBO(28, 28, 30, 0.5),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Text('Spanish',
-                            style: TextStyle(fontSize: 18.0),
-                            textAlign: TextAlign.center),
-                      )),
-                )),
-                Expanded(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text('Spanish',
+                          style: TextStyle(fontSize: 18.0),
+                          textAlign: TextAlign.center),
+                    )),
+              )),
+              Expanded(
+                  child: Padding(
+                padding: EdgeInsets.only(
+                    left: 5.0, top: 15.0, right: 15.0, bottom: 15.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(28, 28, 30, 0.5),
+                        borderRadius: BorderRadius.circular(10.0)),
                     child: Padding(
-                  padding: EdgeInsets.only(
-                      left: 5.0, top: 15.0, right: 15.0, bottom: 15.0),
-                  child: Container(
-                      decoration: BoxDecoration(
-                          color: Color.fromRGBO(28, 28, 30, 0.5),
-                          borderRadius: BorderRadius.circular(10.0)),
-                      child: Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Column(children: [
-                          Text(
-                            'English',
-                            style: TextStyle(fontSize: 18.0),
-                            textAlign: TextAlign.center,
-                          ),
-                          Text(
-                            'UK',
-                            style:
-                                TextStyle(color: Colors.grey, fontSize: 18.0),
-                            textAlign: TextAlign.center,
-                          ),
-                        ]),
-                      )),
-                ))
-              ],
-            ),
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(children: [
+                        Text(
+                          'English',
+                          style: TextStyle(fontSize: 18.0),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'UK',
+                          style: TextStyle(color: Colors.grey, fontSize: 18.0),
+                          textAlign: TextAlign.center,
+                        ),
+                      ]),
+                    )),
+              ))
+            ],
           ),
         ),
-        Expanded(
-          child: Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(28, 28, 30, 0.5),
-                borderRadius: BorderRadius.circular(10.0)),
-            child: Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: FutureBuilder(
-                    future: SharedPref().getFirebaseData('firebase'),
-                    builder: (BuildContext context, AsyncSnapshot snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Center(child: CircularProgressIndicator());
-                      } else {
-                        return TextField(
-                          style: TextStyle(fontSize: 24.0),
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: snapshot.hasData
-                                  ? snapshot.data
-                                  : 'Enter text'),
-                        );
-                      }
-                    })),
-          ),
-        )
-      ],
-    );
-  }
+      ),
+      Expanded(
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: Color.fromRGBO(28, 28, 30, 0.5),
+              borderRadius: BorderRadius.circular(10.0)),
+          child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: FutureBuilder(
+                  future: SharedPref().getFirebaseData('firebase'),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else {
+                      return TextField(
+                        style: TextStyle(fontSize: 24.0),
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: snapshot.hasData
+                                ? snapshot.data
+                                : 'Enter text'),
+                      );
+                    }
+                  })),
+        ),
+      )
+    ],
+  );
 }
