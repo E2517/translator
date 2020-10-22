42667208b13594fc4cdc2378139561b64ad7f6fd
Carlos Caravaca <ccaravac@everis.com>
2020-10-22 10:15:55 +0100

    bottom

--- lib/widgets/bottom_utils.dart
+++ lib/widgets/bottom_utils.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 
 class CustomBottomNavigationBar extends BottomNavigationBar {
   CustomBottomNavigationBar(
-      {Key key, int currentIndex, Function(int index) onTap})
+      {Key key,
+      int currentIndex,
+      String labelOne,
+      String labelTwo,
+      Function(int index) onTap})
       : super(
             key: key,
             fixedColor: Color.fromRGBO(106, 197, 220, 1),
             currentIndex: currentIndex,
             items: [
               BottomNavigationBarItem(
-                  label: 'Translate', icon: Icon(Icons.g_translate)),
+                  label: labelOne ?? 'Translate',
+                  icon: Icon(Icons.g_translate)),
               BottomNavigationBarItem(
-                  label: 'Favourites', icon: Icon(Icons.star_rate)),
+                  label: 'Remove', icon: Icon(Icons.delete_outline)),
+              BottomNavigationBarItem(
+                  label: labelTwo ?? 'Favourites', icon: Icon(Icons.star_rate)),
             ],
             onTap: onTap);
 }
