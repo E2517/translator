340602202687b9a0c498df2517d8b72e4a4d677d
Carlos Caravaca <ccaravac@everis.com>
2020-10-23 16:18:38 +0100

    Favourites

--- lib/views/favourites.views.dart
+++ lib/views/favourites.views.dart
@@ -18,18 +18,118 @@ class _FavouritesState extends State<Favourites> {
           title: Text('Favourites'),
           centerTitle: true,
         ),
-        body: Consumer<TranslateModel>(
-          builder: (context, translate, child) {
-            return FutureBuilder(
-              future: translate.dataFirebase,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                return Center(child: Text(snapshot.data));
-              },
-            );
-          },
+        body: SingleChildScrollView(
+          child: Consumer<TranslateModel>(
+            builder: (context, translate, child) {
+              return FutureBuilder(
+                future: translate.dataFirebase,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.star_rate,
+                          size: 75,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(height: 35.0),
+                        Text('No Favourites',
+                            style:
+                                TextStyle(fontSize: 24.0, color: Colors.grey)),
+                        SizedBox(height: 20.0),
+                        Text('Your Favourite translation will apperar here',
+                            style:
+                                TextStyle(fontSize: 16.0, color: Colors.grey)),
+                      ],
+                    ));
+                  }
+
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(
+                          'Spanish - English',
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                              fontSize: 35.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Color.fromRGBO(28, 28, 30, 0.5),
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 25.0,
+                                    top: 15.0,
+                                    right: 15.0,
+                                    bottom: 15.0),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Spanish',
+                                        style: TextStyle(fontSize: 24.0),
+                                      ),
+                                      SizedBox(height: 10.0),
+                                      Text(
+                                        snapshot.data,
+                                        style: TextStyle(fontSize: 30.0),
+                                      )
+                                    ]),
+                              ),
+                              SizedBox(height: 25.0),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 25.0,
+                                    top: 15.0,
+                                    right: 15.0,
+                                    bottom: 15.0),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'English(UK)',
+                                        style: TextStyle(
+                                            fontSize: 24.0,
+                                            color: Color.fromRGBO(
+                                                106, 197, 220, 1)),
+                                      ),
+                                      SizedBox(height: 10.0),
+                                      Text(
+                                        snapshot.data,
+                                        style: TextStyle(
+                                            fontSize: 30.0,
+                                            color: Color.fromRGBO(
+                                                106, 197, 220, 1)),
+                                      )
+                                    ]),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                },
+              );
+            },
+          ),
         ),
         bottomNavigationBar: CustomBottomNavigationBar(
           currentIndex: 2,
