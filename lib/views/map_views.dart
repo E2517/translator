75298be6482ce070ad746d0d9d2df2b319c9bc95
Carlos Caravaca <ccaravac@everis.com>
2020-10-25 10:36:26 +0000

    MapBox

---
+++ lib/views/map_views.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:translator/widgets/Map/header_map_widget.dart';
+
+class Map extends StatefulWidget {
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  MapboxMapController controller;
+
+  void _onMapCreated(MapboxMapController controller) {
+    this.controller = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final dataPushNotifications = ModalRoute.of(context).settings.arguments;
+
+    Timer(Duration(seconds: 30),
+        () => Navigator.pushReplacementNamed(context, 'home'));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Map'),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          HeaderInfo(),
+          Expanded(
+            child: Container(
+              child: dataPushNotifications != null
+                  ? Center(
+                      child: Text(
+                          'You have a new notification: $dataPushNotifications',
+                          style: TextStyle(fontSize: 24.0),
+                          textAlign: TextAlign.center),
+                    )
+                  : MapboxMap(
+                      accessToken:
+                          'pk.eyJ1IjoiY29uZ29tYXAiLCJhIjoiY2tnb3dzeGNyMGduNzJ6bW4yOXVuZTk2ZyJ9.AjUBf6jX3xmPqdF6ZO5qxQ',
+                      onMapCreated: _onMapCreated,
+                      styleString:
+                          'mapbox://styles/congomap/ckgoy5ok80l6s1apmpcdqiy9n',
+                      initialCameraPosition: const CameraPosition(
+                        target: LatLng(51.50853, -0.12574),
+                        zoom: 11.0,
+                      ),
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
