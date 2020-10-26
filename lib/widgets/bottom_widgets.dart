32c47a2ce3a164416cb6ff0c264a9c33d0b67d87
Carlos Caravaca <ccaravac@everis.com>
2020-10-26 12:24:09 +0000

    routes

--- lib/widgets/bottom_widgets.dart
+++ lib/widgets/bottom_widgets.dart
@@ -5,7 +5,10 @@ class CustomBottomNavigationBar extends BottomNavigationBar {
       {Key key,
       @required int currentIndex,
       String labelOne,
+      String labelTwo,
       String labelThree,
+      Icon iconOne,
+      Icon iconTwo,
       Icon iconThree,
       Function(int index) onTap})
       : super(
@@ -17,7 +20,8 @@ class CustomBottomNavigationBar extends BottomNavigationBar {
                   label: labelOne ?? 'Translate',
                   icon: Icon(Icons.g_translate)),
               BottomNavigationBarItem(
-                  label: 'Remove', icon: Icon(Icons.delete_outline)),
+                  label: labelTwo ?? 'Remove',
+                  icon: iconTwo ?? Icon(Icons.delete_outline)),
               BottomNavigationBarItem(
                   label: labelThree ?? 'Favourites',
                   icon: iconThree ?? Icon(Icons.star_rate)),
