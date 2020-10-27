2a832186d3470c77d405a5c75bc0358b69246ba3
Carlos Caravaca <ccaravac@everis.com>
2020-10-27 10:16:59 +0000

    header icon map

--- lib/widgets/Map/header_map_widget.dart
+++ lib/widgets/Map/header_map_widget.dart
@@ -24,7 +24,7 @@ class HeaderInfo extends StatelessWidget {
             style: TextStyle(color: Colors.black),
           ),
           trailing: Icon(
-            Icons.message_rounded,
+            Icons.g_translate,
             color: Colors.black,
           ),
           onTap: () {
